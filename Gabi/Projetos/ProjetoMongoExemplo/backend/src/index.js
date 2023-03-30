15b2ff39fcc4af240fc433f97da1fe0886d1cb1f
Gabrielle Gomes <128157932+gabriellegomess@users.noreply.github.com>
2023-03-30 10:48:06 -0300

    Banco de Dados
    Json no script

--- Gabi/Projetos/ProjetoMongoExemplo/backend/src/index.js
+++ Gabi/Projetos/ProjetoMongoExemplo/backend/src/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json()); //rota com json
+
+
 app.get("/", (req, res) =>{
-    return res.send("Oi");
+    return res.json({
+        //atributo e valor separados de outro atributo por virgula e o ultimo valor não precisa de , nem de ;
+        nome: "José",
+        idade: 21,
+        sexo: "Masculino"
+    })
 });
 
 
